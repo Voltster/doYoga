697a307ff7b98dd4170fa1d98c454861e5335077
Rohit Kumar Mishra <117002245+Voltster@users.noreply.github.com>
2024-02-20 21:42:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+
+// const scroll = new LocomotiveScroll({
+//     el:document.querySelector("#main"),
+//     smooth:true,
+//     lerp:0.02,
+// })
